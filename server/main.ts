e429034813bf8a4294c56fc59a040aaf98ea24ce
Cactus-0 <cactus1488@proton.me>
2023-04-29 14:25:30 +0300

    add watcher of locales list

--- server/main.ts
+++ server/main.ts
@@ -1,7 +1,10 @@
+import { watch } from 'chokidar';
 import { ConsoleLogger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { join, relative, sep } from 'path';
+import { locales } from './locales';
 
 
 async function main() {
@@ -20,6 +23,23 @@ async function main() {
 	app.useGlobalPipes(new ValidationPipe);
 
 	await app.listen(process.env.PORT);
+
+	if (process.env.NODE_ENV === 'development') {
+		const watchPath = join(__dirname, '..', 'client');
+
+		logger.log(`Working directory: ${process.cwd()}`);
+		logger.log(`Watching: ${relative(process.cwd(), watchPath)}`);
+		
+		watch(watchPath).on('addDir', (newDir) => {
+			const [ dir ] = relative(watchPath, newDir).split(sep);
+			
+			if (dir.length === 0) return;
+			if (locales.includes(dir)) return;
+
+			logger.log(`New locale detected: ${dir}, updating locales list`);
+			locales.push(dir);
+		});
+	}
 }
 
 
