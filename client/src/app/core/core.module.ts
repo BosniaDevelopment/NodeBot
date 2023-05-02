8a0397a6352d7a1cec15f1d4ade328d8b1e54ebd
Cactus-0 <cactus1488@proton.me>
2023-05-02 22:29:43 +0300

    add prallax effect to image on homepage

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { AccessTokenService, ApiService, GuildConfigService, UserService } from './services';
+import { ParallaxDirective } from './directives/parallax-effect.directive';
 
 @NgModule({
-	declarations: [],
-	providers: [
-		AccessTokenService,
-		ApiService,
-		UserService,
-		GuildConfigService
-	],
+	declarations: [ParallaxDirective],
+	providers: [AccessTokenService, ApiService, UserService, GuildConfigService],
 	imports: [CommonModule, HttpClientModule],
+	exports: [ParallaxDirective],
 })
 export class CoreModule {}
