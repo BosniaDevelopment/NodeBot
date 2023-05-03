29e96f8e39a0bd5edca849939659bc990277ca5d
BarsTiger <kit.barsic@gmail.com>
2023-05-03 15:11:47 +0300

    Developing internal api

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,14 @@
 def start():
-    from .config import TOKEN
     bot.run(TOKEN)
 
 
 def main():
+    from rich import print
     print('[italic]Preparing[/]')
 
     from .common import NodeBot
+    from .config import TOKEN
 
     print('[bold]Starting bot...[/]')
 
-    exec(start.__code__, {'bot': NodeBot} | globals() | locals())
+    exec(start.__code__, {'bot': NodeBot, 'TOKEN': TOKEN} | globals() | locals())
